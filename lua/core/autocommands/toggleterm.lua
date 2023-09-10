ed72d6a4918c21c6d8a4f5c4f9d0164e88c6ae60
Joan López <juanlopez4691@gmail.com>
2023-09-10 19:13:58 +0200

    refactor: change cursor shape in terminal attending to editor mode

--- lua/core/autocommands/toggleterm.lua
+++ lua/core/autocommands/toggleterm.lua
@@ -1,13 +1,10 @@
+-- Change cursor shape in terminal attending to editor mode.
+vim.api.nvim_set_hl(0, 'TerminalCursorShape', { underline = true })
 vim.api.nvim_create_autocmd(
+  'TermEnter',
   {
     callback = function()
+      vim.cmd [[setlocal winhighlight=TermCursor:TerminalCursorShape]]
+    end,
   }
 )
-
--- Change cursor shape in terminal attending to mode.
-vim.api.nvim_set_hl(0, 'TerminalCursorShape', { underline = true })
-vim.api.nvim_create_autocmd('TermEnter', {
-  callback = function()
-    vim.cmd [[setlocal winhighlight=TermCursor:TerminalCursorShape]]
-  end,
-})
