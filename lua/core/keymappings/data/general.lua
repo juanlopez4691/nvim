9ea1b930174e8da0e58606dd6838f72c319642ca
Joan López <juanlopez4691@gmail.com>
2023-09-16 14:01:16 +0200

    feat: add move lines(s) up/down

--- lua/core/keymappings/data/general.lua
+++ lua/core/keymappings/data/general.lua
@@ -164,5 +164,55 @@ return {
       desc = 'Indent out',
     },
   },
+
+  -- Move line(s) up/down.
+  {
+    modes = { 'n' },
+    key = '<A-j>',
+    cmd = ':m+<CR>==',
+    opt = {
+      desc = 'Move current line down',
+    },
+  },
+  {
+    modes = { 'n' },
+    key = '<A-k>',
+    cmd = ':m-2<CR>==',
+    opt = {
+      desc = 'Move current line up',
+    },
+  },
+  {
+    modes = { 'i' },
+    key = '<A-j>',
+    cmd = '<ESC>:m+<CR>==gi',
+    opt = {
+      desc = 'Move current line down',
+    },
+  },
+  {
+    modes = { 'i' },
+    key = '<A-k>',
+    cmd = '<ESC>:m-2<CR>==gi',
+    opt = {
+      desc = 'Move current line up',
+    },
+  },
+  {
+    modes = { 'v' },
+    key = '<A-j>',
+    cmd = ":m'>+<CR>gv=gv",
+    opt = {
+      desc = 'Move current line down',
+    },
+  },
+  {
+    modes = { 'v' },
+    key = '<A-k>',
+    cmd = ":m-2<CR>gv=gv",
+    opt = {
+      desc = 'Move current line down',
+    },
+  },
 }
 
