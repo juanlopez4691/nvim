24c4d3a03bc6bc036489814ecb0238d40ec18703
Joan López <juanlopez4691@gmail.com>
2023-10-15 13:17:22 +0200

    feat: remove theme rebelot/kanagawa

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,6 +1,41 @@
 local M = {}
 
-M.colorscheme = 'tokyonight-night'
+-- Plugins to enable.
+M.plugins_enabled = {
+  aerial = true,
+  colorful_winsep = true,
+  comments = true,
+  difview = true,
+  flatten = true,
+  gitsigns = true,
+  indent_blankline = true,
+  lspkind = true,
+  lsp_zero = true,
+  lualine = true,
+  mini_indentscope = true,
+  mini_surround = true,
+  nvim_autopairs = true,
+  nvim_cmp = true,
+  nvim_colorizer = true,
+  nvim_spectre = true,
+  nvim_tree = true,
+  nvim_treesitter = true,
+  nvim_ufo = true,
+  telescope = true,
+  toggleterm = true,
+  trouble = true,
+}
+
+M.colorscheme_default = 'habamax'
+
+-- M.colorscheme = 'tokyonight'
+-- M.colorscheme_variant = 'tokyonight-night'
+
+M.colorscheme = 'kanagawa'
+M.colorscheme_variant = 'kanagawa-wave'
+
+M.plugins_enabled.tokyonight = M.colorscheme == 'tokyonight'
+M.plugins_enabled.kanagawa = M.colorscheme == 'kanagawa'
 
 -- Set global limits for large files.
 M.max_file = {
--- lua/plugins/kanagawa.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    'rebelot/kanagawa.nvim',
-    priority = 10000, -- make sure to load this before all the other start plugins
-  },
-}
-
