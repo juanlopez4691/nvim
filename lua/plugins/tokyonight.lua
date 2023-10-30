8f0bfeeacce8e505afc292aba5cfb449402fcdd3
Joan López <juanlopez4691@gmail.com>
2023-10-30 10:44:14 +0100

    refactor: loading of color scheme tokyonight

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,7 +1,5 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    priority = 10000, -- make sure to load this before all the other start plugins
-  },
+  'folke/tokyonight.nvim',
+  priority = 10000, -- make sure to load this before all the other start plugins
 }
 
