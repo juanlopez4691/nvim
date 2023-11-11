c0e184a7ff055d006930eb5e39b8d9825ddb2199
Joan López <juanlopez4691@gmail.com>
2023-11-11 17:09:32 +0100

    fix: mason not available on start up

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -2,13 +2,14 @@ return {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v2.x',
   cmd = 'LspInfo',
-  event = 'VeryLazy',
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     require('lsp-zero.settings').preset({})
   end,
   dependencies = {
     -- LSP Support
     { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
     {
       'williamboman/mason-lspconfig.nvim',
       config = function()
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,7 @@
 return {
   'williamboman/mason.nvim',
   lazy = true,
+  cmd = 'Mason',
   config = function()
     require('mason').setup({
       ui = {
