5059ab1e7049098e320f45cbcfc3db813b7b95c0
Joan López <juanlopez4691@gmail.com>
2023-11-19 20:26:05 +0100

    fix: show current line number correctly

--- lua/core/autocommands/linenumbers.lua
+++ lua/core/autocommands/linenumbers.lua
@@ -1,14 +1,17 @@
+local opt = vim.opt
+
 -- Automatically toggle between relative and absolute line numbers.
 local augroup = vim.api.nvim_create_augroup('line_number_toggle', {})
 
 vim.api.nvim_create_autocmd(
-  {'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter'},
+  { 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' },
   {
     pattern = '*',
     group = augroup,
     callback = function()
       if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then
-        vim.opt.relativenumber = true
+        opt.relativenumber = true
+        opt.statuscolumn = '%=%{v:relnum ? v:relnum : v:lnum }%s%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "" : "") : " " }'
       end
     end,
   }
@@ -21,7 +24,8 @@ vim.api.nvim_create_autocmd(
     group = augroup,
     callback = function()
       if vim.o.nu then
-        vim.opt.relativenumber = false
+        opt.relativenumber = false
+        opt.statuscolumn = '%=%l%s%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "" : "") : " " }'
         vim.cmd 'redraw'
       end
     end,
