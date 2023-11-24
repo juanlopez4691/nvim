c1e56199b5219905c80b1ad1e0083d2fb61b0408
Joan López <juanlopez4691@gmail.com>
2023-11-24 11:07:38 +0100

    feat: hide status column on terminal buffers

--- lua/core/autocommands/toggleterm.lua
+++ lua/core/autocommands/toggleterm.lua
@@ -1,9 +1,26 @@
+local opt = vim.opt
+local augroup = vim.api.nvim_create_augroup('terminal', {})
+
 -- Change cursor shape in terminal attending to editor mode.
 vim.api.nvim_set_hl(0, 'TerminalCursorShape', { underline = true })
+
+vim.api.nvim_create_autocmd(
+  'TermOpen',
+  {
+    group = augroup,
+    callback = function()
+      opt.statuscolumn = ''
+    end,
+  }
+)
+
 vim.api.nvim_create_autocmd(
   'TermEnter',
   {
+    group = augroup,
     callback = function()
+      opt.statuscolumn = ''
+      vim.cmd [[setlocal signcolumn=no]]
       vim.cmd [[setlocal winhighlight=TermCursor:TerminalCursorShape]]
     end,
   }
