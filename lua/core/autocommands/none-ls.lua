d0cbdad263840dae861969d4591bdf0fbe5c69f2
Joan López <juanlopez4691@gmail.com>
2023-11-27 19:56:46 +0100

    fix: control code formatting on save with related setting

--- lua/core/autocommands/none-ls.lua
+++ lua/core/autocommands/none-ls.lua
@@ -1,24 +1,21 @@
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-
-if not _G.Settings.plugins_enabled.none_ls then
-  return
-end
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- Based on null-ls documentation.
 -- See https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#code
 -- See https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#choosing-a-client-for-formatting
-vim.api.nvim_create_autocmd(
-  { 'BufWritePre' },
-  {
-    pattern = '*',
-    group = augroup,
-    callback = function()
-      vim.lsp.buf.format({
-        async = false,
-        filter = function(client)
-          return client.name == 'null-ls'
-        end
-      })
-    end,
-  }
-)
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = "*",
+	group = augroup,
+	callback = function()
+		if not _G.Settings.plugins_enabled.none_ls or not _G.Settings.format_on_save then
+			return
+		end
+
+		vim.lsp.buf.format({
+			async = false,
+			filter = function(client)
+				return client.name == "null-ls"
+			end,
+		})
+	end,
+})
