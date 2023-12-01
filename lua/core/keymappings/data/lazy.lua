998a46dddd0a5ef0ff475bb679d026c2c84b41bf
Joan López <juanlopez4691@gmail.com>
2023-12-01 12:37:08 +0100

    feat: add keymaps to manage packages with lazy

--- lua/core/keymappings/data/lazy.lua
+++ lua/core/keymappings/data/lazy.lua
@@ -1,19 +1,43 @@
 return {
-	groups = {
-		{
-			whichkey = true,
-			prefix = "<leader>P",
-			name = "+Packages",
-			mappings = {
-				{
-					modes = { "n" },
-					key = "<leader>Pl",
-					cmd = "<cmd>Lazy<cr>",
-					opt = {
-						desc = "Open Lazy",
-					},
-				},
-			},
-		},
-	},
+  groups = {
+    {
+      whichkey = true,
+      prefix = "<leader>P",
+      name = "+Packages",
+      mappings = {
+        {
+          modes = { "n" },
+          key = "<leader>Pl",
+          cmd = "<cmd>Lazy<cr>",
+          opt = {
+            desc = "Open Lazy",
+          },
+        },
+        {
+          modes = { "n" },
+          key = "<leader>Pc",
+          cmd = "<cmd>Lazy check<cr>",
+          opt = {
+            desc = "Check packages",
+          },
+        },
+        {
+          modes = { "n" },
+          key = "<leader>Ps",
+          cmd = "<cmd>Lazy sync<cr>",
+          opt = {
+            desc = "Sync packages",
+          },
+        },
+        {
+          modes = { "n" },
+          key = "<leader>Pp",
+          cmd = "<cmd>Lazy profile<cr>",
+          opt = {
+            desc = "Profile",
+          },
+        },
+      },
+    },
+  },
 }
