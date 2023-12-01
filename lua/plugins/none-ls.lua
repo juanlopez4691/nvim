dabf7375d97be909fa20c87244a240645820f19c
Joan López <juanlopez4691@gmail.com>
2023-12-01 11:26:47 +0100

    feat: ensure non-ls tries local executables to lint and fix PHP code

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,25 +1,42 @@
-local core_settings = require("core.settings")
+local phpcs_exec = "phpcs"
+local phpcbf_exec = "phpcbf"
+local vendor_path = "./vendor/bin/"
+
+if vim.fn.executable(vendor_path .. phpcs_exec) == 1 then
+  phpcs_exec = vendor_path .. phpcs_exec
+end
+
+if vim.fn.executable(vendor_path .. phpcbf_exec) == 1 then
+  phpcbf_exec = vendor_path .. phpcbf_exec
+end
 
 return {
-	"nvimtools/none-ls.nvim",
-	cond = _G.Settings.plugins_enabled.none_ls,
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local nls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  cond = _G.Settings.plugins_enabled.none_ls,
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local nls = require("null-ls")
 
-		local sources = {
-			nls.builtins.formatting.stylua,
-			nls.builtins.formatting.prettierd,
-			nls.builtins.diagnostics.eslint_d,
-			nls.builtins.diagnostics.stylelint,
-			nls.builtins.formatting.rustywind,
-			nls.builtins.completion.spell,
-			nls.builtins.diagnostics.php,
-			nls.builtins.code_actions.gitsigns,
-		}
-		nls.setup({
-			sources = sources,
-		})
-	end,
+    local sources = {
+      nls.builtins.formatting.stylua,
+      nls.builtins.formatting.prettierd,
+      nls.builtins.diagnostics.eslint_d,
+      nls.builtins.diagnostics.stylelint,
+      nls.builtins.formatting.rustywind,
+      nls.builtins.completion.spell,
+      nls.builtins.diagnostics.php,
+      nls.builtins.diagnostics.phpcs.with({
+        command = phpcs_exec,
+      }),
+      nls.builtins.diagnostics.phpstan,
+      nls.builtins.formatting.phpcbf.with({
+        command = phpcbf_exec,
+      }),
+      nls.builtins.code_actions.gitsigns,
+    }
+    nls.setup({
+      sources = sources,
+    })
+  end,
 }
