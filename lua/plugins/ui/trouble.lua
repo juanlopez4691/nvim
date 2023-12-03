8103f88bfd2fdcef03803cd9a7e65eeef80a2b85
Joan López <juanlopez4691@gmail.com>
2023-12-03 13:17:42 +0100

    refactor: group ui related plugins

--- lua/plugins/ui/trouble.lua
+++ lua/plugins/ui/trouble.lua
@@ -2,6 +2,6 @@ return {
   "folke/trouble.nvim",
   cond = _G.Settings.plugins_enabled.trouble,
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  cmd = 'TroubleToggle',
+  cmd = "TroubleToggle",
   opts = {},
 }
