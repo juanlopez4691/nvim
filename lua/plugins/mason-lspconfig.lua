deed638c2cd42adb6e1149e6d2279a22eb4a980c
Joan López <juanlopez4691@gmail.com>
2023-12-03 12:33:08 +0100

    feat[mason-lspconfig]: add ensure installed language servers

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -2,6 +2,27 @@ return {
   "williamboman/mason-lspconfig.nvim",
   lazy = true,
   config = function()
-    require("mason-lspconfig").setup({})
+    require("mason-lspconfig").setup({
+      ensure_installed = {
+        "bashls",
+        "cssls",
+        "dockerls",
+        "docker_compose_language_service",
+        "efm",
+        "emmet_ls",
+        "eslint",
+        "html",
+        "intelephense",
+        "jsonls",
+        "lemminx",
+        "lua_ls",
+        "sqlls",
+        "tailwindcss",
+        "taplo",
+        "tsserver",
+        "vimls",
+        "yamlls",
+      },
+    })
   end,
 }
