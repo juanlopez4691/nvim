5c00abeb4c63a6b048c3fcede2d657477816028c
Joan López <juanlopez4691@gmail.com>
2023-12-03 13:27:02 +0100

    refactor: group telescope related plugins

--- lua/plugins/telescope-live-grep-args.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'nvim-telescope/telescope-live-grep-args.nvim',
-  version = '^1.0.0',
-  lazy = true,
-}
-
--- lua/plugins/telescope/dependencies.lua
+++ lua/plugins/telescope/dependencies.lua
@@ -1,14 +1,22 @@
 return {
   {
-    'nvim-lua/plenary.nvim',
-    config = function ()
-      require'plenary.filetype'.add_file('filetypes')
-    end
+    "nvim-lua/plenary.nvim",
+    config = function()
+      require("plenary.filetype").add_file("filetypes")
+    end,
   },
-  { 'nvim-tree/nvim-web-devicons' },
+  { "nvim-tree/nvim-web-devicons" },
   -- Telescope extensions.
-  { 'debugloop/telescope-undo.nvim' },
-  { 'nvim-telescope/telescope-media-files.nvim' },
-  { 'nvim-telescope/telescope-live-grep-args.nvim' },
-  { 'nvim-telescope/telescope-fzf-native.nvim' },
+  { "debugloop/telescope-undo.nvim" },
+  { "nvim-telescope/telescope-media-files.nvim" },
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    version = "^1.0.0",
+    lazy = true,
+  },
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+    lazy = true,
+  },
 }
