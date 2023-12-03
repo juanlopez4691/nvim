1848f0cf35db92d2fd307e1d95a223f40c3e9ccf
Joan López <juanlopez4691@gmail.com>
2023-12-03 20:11:21 +0100

    refactor: fix code style

--- lua/core/keymappings/data/trouble.lua
+++ lua/core/keymappings/data/trouble.lua
@@ -2,18 +2,18 @@ return {
   groups = {
     {
       whichkey = true,
-      prefix = '<leader>x',
-      name = 'Trouble',
+      prefix = "<leader>x",
+      name = "Trouble",
       mappings = {
         {
-          modes = { 'n' },
-          key = '<leader>xx',
-          cmd = '<cmd>TroubleToggle<CR>',
+          modes = { "n" },
+          key = "<leader>xx",
+          cmd = "<cmd>TroubleToggle<CR>",
           opt = {
-            desc = 'Trouble toggle',
+            desc = "Trouble toggle",
           },
         },
       },
-    }
+    },
   },
 }
