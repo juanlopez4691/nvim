63e3cd509a64f7a21094cf0b54d85b82faa7d389
Joan López <juanlopez4691@gmail.com>
2023-12-05 20:36:12 +0100

    refactor: fix code style

--- lua/core/keymappings/data/lazygit.lua
+++ lua/core/keymappings/data/lazygit.lua
@@ -2,15 +2,15 @@ return {
   groups = {
     {
       whichkey = true,
-      prefix = '<leader>g',
-      name = 'Git',
+      prefix = "<leader>g",
+      name = "Git",
       mappings = {
         {
-          modes = { 'n' },
-          key = '<leader>gg',
+          modes = { "n" },
+          key = "<leader>gg",
           cmd = "<cmd>lua TerminalFullWindow('lazygit --use-config-file=$HOME/.config/lazygit/config.yml')<CR>",
           opt = {
-            desc = 'Open lazygit',
+            desc = "Open lazygit",
           },
         },
       },
