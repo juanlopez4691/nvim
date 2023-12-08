f59902443abfacb3060b47d5431f0226fd5425fd
Joan López <juanlopez4691@gmail.com>
2023-12-08 17:26:40 +0100

    feat: update help related keymappings

--- lua/core/keymappings/data/telescope.lua
+++ lua/core/keymappings/data/telescope.lua
@@ -150,14 +150,22 @@ return {
     {
       whichkey = true,
       prefix = "<leader>h",
-      name = "Help",
+      name = "+Help",
       mappings = {
         {
           modes = { "n" },
-          key = "<leader>h",
+          key = "<leader>hh",
+          cmd = "<cmd>help<CR>",
+          opt = {
+            desc = "Help",
+          },
+        },
+        {
+          modes = { "n" },
+          key = "<leader>hs",
           cmd = "<cmd>Telescope help_tags prompt_prefix=💡›<CR>",
           opt = {
-            desc = "Help pages",
+            desc = "Help search",
           },
         },
       },
