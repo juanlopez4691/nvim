6f50bc4f5ce5e894eb31b026eec6db52155b8676
Joan López <juanlopez4691@gmail.com>
2023-12-10 17:02:37 +0100

    refactor: fix code style

--- lua/core/helpers.lua
+++ lua/core/helpers.lua
@@ -4,15 +4,16 @@ function M:new()
   return {}
 end
 
+-----------------------------------------------------------
 -- Helper method to set keys remaps.
+-----------------------------------------------------------
 M.map = function(mode, lhs, rhs, opts)
   local options = { noremap = true, silent = true }
 
   if opts then
-    options = vim.tbl_extend('force', options, opts)
+    options = vim.tbl_extend("force", options, opts)
   end
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
 return M
-
