b7f42d83ba5be15ba7e34724af94ace90900b0b2
Joan López <juanlopez4691@gmail.com>
2023-12-19 10:44:24 +0100

    feat[nvim-treesitter]: add keymappings for incremental selection

--- lua/plugins/code/treesitter/nvim-treesitter.lua
+++ lua/plugins/code/treesitter/nvim-treesitter.lua
@@ -61,6 +61,11 @@ return {
       },
       incremental_selection = {
         enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
         },
       },
       sync_install = true,
