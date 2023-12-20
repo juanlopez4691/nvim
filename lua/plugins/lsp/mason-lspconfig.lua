7d45c8b17deba8b719baac577a5dcb2dcf4db212
Joan López <juanlopez4691@gmail.com>
2023-12-20 19:28:33 +0100

    feat[mason-lspconfig]: add lsp handler for lua

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -2,7 +2,16 @@ return {
   "williamboman/mason-lspconfig.nvim",
   lazy = true,
   config = function()
+    local lsp_zero = require("lsp-zero")
+
     require("mason-lspconfig").setup({
+      handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          require("lspconfig").lua_ls.setup(lua_opts)
+        end,
+      },
       ensure_installed = {
         "bashls",
         "cssls",
