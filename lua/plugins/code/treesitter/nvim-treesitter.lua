aaf37823398e87e9190418562ca0f79dd1b26c43
Joan López <juanlopez4691@gmail.com>
2023-12-25 12:33:29 +0100

    feat[treesitter]: add keymappings to manage text objects

--- lua/plugins/code/treesitter/nvim-treesitter.lua
+++ lua/plugins/code/treesitter/nvim-treesitter.lua
@@ -55,9 +55,53 @@ return {
         additional_vim_regex_highlighting = {
           "php",
         },
+        indent = {
+          enabled = true,
+        },
       },
-      indent = {
-        enabled = true,
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- Whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>ea"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>eA"] = "@parameter.inner",
+          },
+        },
       },
       incremental_selection = {
         enable = true,
