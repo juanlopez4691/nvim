f68f729b4772c7e02f791cf77a42e0675999c65e
Joan López <juanlopez4691@gmail.com>
2023-12-25 11:38:16 +0100

    feat: add code diagnostics keymappings

--- lua/core/keymappings/data/diagnostics.lua
+++ lua/core/keymappings/data/diagnostics.lua
@@ -2,10 +2,48 @@ return {
   groups = {
     {
       enabled = true,
+      whichkey = false,
+      mappings = {
+        {
+          modes = { "n" },
+          key = "]d",
+          cmd = "<cmd>lua vim.diagnostic.goto_next()<CR>",
+          opt = {
+            desc = "Next diagnostic",
+          },
+        },
+        {
+          modes = { "n" },
+          key = "[d",
+          cmd = "<cmd>lua vim.diagnostic.goto_prev()<CR>",
+          opt = {
+            desc = "Previous diagnostic",
+          },
+        },
+      },
+    },
+    {
+      enabled = true,
       whichkey = true,
       prefix = "<leader>d",
       name = "+Diagnostics",
       mappings = {
+        {
+          modes = { "n" },
+          key = "<leader>de",
+          cmd = "<cmd>lua vim.diagnostic.open_float()<CR>",
+          opt = {
+            desc = "Open float diagnostics message",
+          },
+        },
+        {
+          modes = { "n" },
+          key = "<leader>dq",
+          cmd = "<cmd>lua vim.diagnostic.setloclist()<CR>",
+          opt = {
+            desc = "Open diagnostics list",
+          },
+        },
         {
           enabled = _G.Settings.plugins_enabled.touble,
           modes = { "n" },
