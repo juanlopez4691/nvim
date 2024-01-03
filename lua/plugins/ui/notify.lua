66656780f86c46c846de916d1cc87f51cf68a38c
Joan López <juanlopez4691@gmail.com>
2024-01-03 18:21:42 +0100

    feat: add plugin liangxianzhe/floating-input.nvim

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -2,6 +2,9 @@ return {
   "rcarriga/nvim-notify",
   event = "VeryLazy",
   cond = _G.Settings.plugins_enabled.notify,
+  dependencies = {
+    "liangxianzhe/floating-input.nvim",
+  },
   config = function()
     local notify = require("notify")
 
@@ -9,7 +12,15 @@ return {
       timeout = 3000,
       stages = "slide",
     })
+
     vim.notify = notify
+    vim.ui.input = function(opts, on_confirm)
+      require("floating-input").input(opts, on_confirm, {
+        relative = "win",
+        title_pos = "center",
+      })
+    end
+
     require("telescope").load_extension("notify")
   end,
 }
