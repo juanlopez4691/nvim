5b4a5f47559e37086a9ef6421b7bf1df8780cbd1
Joan López <juanlopez4691@gmail.com>
2024-01-10 22:52:05 +0100

    feat: highlight cursor line only on active window

--- lua/core/autocommands/general.lua
+++ lua/core/autocommands/general.lua
@@ -14,3 +14,16 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<cmd>q<cr>", { buffer = event.buf, silent = true })
   end,
 })
+
+-- Set cursor line only in active window.
+vim.api.nvim_create_autocmd("WinLeave", {
+  group = augroup,
+  pattern = "*",
+  command = "setlocal nocursorline",
+})
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  group = augroup,
+  pattern = "*",
+  command = "setlocal cursorline cursorlineopt=both",
+})
