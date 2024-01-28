3aa23c9edc8d5e6f9cb5a0f2b965534220582708
Joan López <juanlopez4691@gmail.com>
2024-01-28 12:26:59 +0100

    feat: add plugin mfussenegger/nvim-lint

---
+++ lua/plugins/code/nvim-lint.lua
@@ -0,0 +1,56 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      css = { "stylelint" },
+      javascript = { "eslint_d", "eslint" },
+      javascriptreact = { "eslint_d", "eslint" },
+      lua = { "luacheck" },
+      scss = { "stylelint" },
+      typescript = { "eslint_d", "eslint" },
+      typescriptreact = { "eslint_d", "eslint" },
+    }
+
+    -- Avoid errors thrown by global vim variables.
+    require("lint").linters.luacheck.args = {
+      globals = {
+        "vim",
+      },
+    }
+
+    local phpcs_exec = "phpcs"
+    local vendor_path = "./vendor/bin/"
+    local phpcs = require("lint").linters.phpcs
+
+    if vim.fn.executable(vendor_path .. phpcs_exec) == 1 then
+      phpcs_exec = vendor_path .. phpcs_exec
+    else
+      phpcs_exec = ""
+    end
+
+    if phpcs_exec == "" then
+      lint.linters_by_ft.php = { "phpstan", "php" }
+
+      phpcs.args = {
+        "-q",
+        -- <- Add a new parameter here
+        "--standard=PSR12",
+        "--report=json",
+        "-",
+      }
+    else
+      lint.linters_by_ft.php = { "phpcs", "phpstan", "php" }
+
+      phpcs.cmd = phpcs_exec
+      phpcs.args = {
+        "-q",
+        "--report=json",
+        "-",
+        "--standard=phpcs.xml",
+      }
+    end
+  end,
+}
