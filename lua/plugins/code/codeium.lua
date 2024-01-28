5060d36571b899218d2170b43af9313ff979f325
Joan López <juanlopez4691@gmail.com>
2024-01-28 19:25:24 +0100

    fix: codeium fails to start
    Issue reported and closed in https://github.com/Exafunction/codeium.vim/issues/308

--- lua/plugins/code/codeium.lua
+++ lua/plugins/code/codeium.lua
@@ -3,6 +3,7 @@ return {
   cond = _G.Settings.plugins_enabled.codeium,
   event = "InsertEnter",
   build = ":Codeium Auth",
+  branch = "main",
   config = function()
     -- Press <C-g> to accept Codeium suggestion.
     vim.keymap.set("i", "<C-g>", function()
