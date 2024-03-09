9e7865d18c445e82089f0b7731dae655dee8c550
Joan López <juanlopez4691@gmail.com>
2024-03-09 13:02:08 +0100

    fix[colorful-winsep]: wrong win separator first time file tree is opened

--- lua/plugins/ui/colorful-winsep.lua
+++ lua/plugins/ui/colorful-winsep.lua
@@ -12,7 +12,12 @@ return {
       -- timer refresh rate
       interval = 30,
       -- This plugin will not be activated for filetype in the following table.
-      no_exec_files = { "packer", "TelescopePrompt", "mason", "CompetiTest", "NvimTree" },
+      no_exec_files = {
+        "packer",
+        "TelescopePrompt",
+        "mason",
+        "CompetiTest",
+      },
       -- Symbols for separator lines, the order: horizontal, vertical, top left, top right, bottom left, bottom right.
       symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
       smooth = false,
