c82235eae49d87b718a60d8c03b90c5393e5d106
Joan López <juanlopez4691@gmail.com>
2024-03-09 15:00:57 +0100

    refactor: improve keymappings for codeium completions

--- lua/plugins/code/codeium.lua
+++ lua/plugins/code/codeium.lua
@@ -8,6 +8,18 @@ return {
     -- Press <C-g> to accept Codeium suggestion.
     vim.keymap.set("i", "<C-g>", function()
       return vim.fn["codeium#Accept"]()
-    end, { expr = true, silent = true })
+    end, { expr = true, silent = true, desc = "Codeium: Accept" })
+
+    vim.keymap.set("i", "<M-]>", function()
+      return vim.fn["codeium#CycleCompletions"](1)
+    end, { expr = true, silent = true, desc = "Codeium: Next completion" })
+
+    vim.keymap.set("i", "<M-[>", function()
+      return vim.fn["codeium#CycleCompletions"](-1)
+    end, { expr = true, silent = true, desc = "Codeium: Previous completion" })
+
+    vim.keymap.set("i", "<C-]>", function()
+      return vim.fn["codeium#Clear"](-1)
+    end, { expr = true, silent = true, desc = "Codeium: Clears completions" })
   end,
 }
