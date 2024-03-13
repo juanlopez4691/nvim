e8e5efd64f062786402722867ae3691fb99ec7a7
Joan López <juanlopez4691@gmail.com>
2024-03-13 14:07:56 +0100

    fix[visual-surround]: conflicts with native keymappings.

--- lua/plugins/code/visual-surround.lua
+++ lua/plugins/code/visual-surround.lua
@@ -3,6 +3,20 @@ return {
   cond = _G.Settings.plugins_enabled.surround,
   event = { "BufReadPre", "BufNewFile" },
   config = function()
-    require("visual-surround").setup({})
+    require("visual-surround").setup({
+      use_default_keymaps = false,
+    })
+
+    local preffered_mapping_prefix = "s"
+    local surround_chars = {
+      "{", "[", "(", "'", '"', '`', '\\', "/", "*", "#", "~", "@", "<"
+    }
+    local surround = require("visual-surround").surround
+
+    for _, key in pairs(surround_chars) do
+      vim.keymap.set("v", preffered_mapping_prefix .. key, function()
+        surround(key)
+      end, { desc = "[visual-surround] Surround selection with " .. key })
+    end
   end,
 }
