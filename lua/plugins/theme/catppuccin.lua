ca4ebd3b8e68c693bd72cb17e2a356962481da60
Joan López <juanlopez4691@gmail.com>
2024-03-19 09:48:35 +0100

    feat: improve configuration of colorscheme catppuccin

--- lua/plugins/theme/catppuccin.lua
+++ lua/plugins/theme/catppuccin.lua
@@ -2,4 +2,23 @@ return {
   "catppuccin/nvim",
   lazy = false,
   priority = 10000, -- make sure to load this before all the other start plugins
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = true,
+          scope_color = "lavender",
+        },
+        treesitter = true,
+        lsp_trouble = true,
+        notify = true,
+        nvimtree = true,
+        which_key = true,
+      },
+    })
+  end,
 }
