e47f54c0a5bffecf1889c8b700d23874320375d9
Joan López <juanlopez4691@gmail.com>
2024-03-28 10:30:54 +0100

    refactor[nvim-dap-ui]: toggle dap-ui when dap session is started/terminated

--- lua/plugins/code/debug/nvim-dap-ui.lua
+++ lua/plugins/code/debug/nvim-dap-ui.lua
@@ -81,17 +81,22 @@ return {
 
     dapui.setup(opts)
 
-    ---@diagnostic disable-next-line: undefined-field
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open({})
+    -- Open dap-ui when dap session is started.
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
     end
-    ---@diagnostic disable-next-line: undefined-field
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close({})
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
     end
-    ---@diagnostic disable-next-line: undefined-field
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close({})
+
+    -- Close dap-ui when dap session is terminated.
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
     end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    ---@diagnostic disable-next-line: undefined-field
   end,
 }
