a784713235312f127d8b181b4664541e1de720d8
Joan López <juanlopez4691@gmail.com>
2024-03-31 06:27:47 +0200

    refactor: plugin nvim-zh/colorful-winsep config

--- lua/plugins/ui/colorful-winsep.lua
+++ lua/plugins/ui/colorful-winsep.lua
@@ -1,25 +1,9 @@
 return {
   "nvim-zh/colorful-winsep.nvim",
   cond = _G.Settings.plugins_enabled.colorful_winsep,
-  event = "VeryLazy",
+  event = "WinNew",
   config = function()
     require("colorful-winsep").setup({
-      -- highlight for Window separator
-      highlight = {
-        bg = "#16161E",
-        fg = "#32566E",
-      },
-      -- timer refresh rate
-      interval = 30,
-      -- This plugin will not be activated for filetype in the following table.
-      no_exec_files = {
-        "packer",
-        "TelescopePrompt",
-        "mason",
-        "CompetiTest",
-      },
-      -- Symbols for separator lines, the order: horizontal, vertical, top left, top right, bottom left, bottom right.
-      symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
       smooth = false,
     })
   end,
