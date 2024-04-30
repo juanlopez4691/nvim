0a265f2b93c90a1471dfae3859ccbbdf93778247
Joan López <juanlopez4691@gmail.com>
2024-04-30 12:56:55 +0200

    fix[incline]: error thrown if plugin nvim-web-devicons is missing

--- lua/plugins/ui/incline.lua
+++ lua/plugins/ui/incline.lua
@@ -3,22 +3,31 @@ return {
   "b0o/incline.nvim",
   event = "VeryLazy",
   config = function()
-    local devicons = require("nvim-web-devicons")
-
     require("incline").setup({
       window = {
         padding = 0,
         margin = { horizontal = 0 },
       },
       render = function(props)
+        local devicons_ok, devicons = pcall(require, "nvim-web-devicons")
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
         if filename == "" then
           filename = "[No Name]"
         end
-        local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+        local ft_color = ""
+        local ft_icon = ""
+        local icon = {}
+
+        if devicons_ok then
+          ft_icon, ft_color = devicons.get_icon_color(filename)
+          icon = ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = "#333333" } or ""
+        end
+
         local modified = vim.bo[props.buf].modified
+
         return {
-          ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = "#333333" } or "",
+          icon,
           " ",
           { filename, gui = modified and "bold,italic" or "bold" },
           " ",
