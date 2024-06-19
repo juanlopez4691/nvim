8598e37250ee4ae9fe0da2347a12cf454836e65e
Joan López <juanlopez4691@gmail.com>
2024-06-19 18:42:20 +0200

    feat: set project root folder for tailwindcss lsp server

--- lua/plugins/lsp/inc/tailwindcss.lua
+++ lua/plugins/lsp/inc/tailwindcss.lua
@@ -3,7 +3,11 @@ local lspconfig = require("lspconfig")
 local M = {}
 
 M.setup = function()
-  lspconfig.tailwindcss.setup({})
+  lspconfig.tailwindcss.setup({
+    root_dir = function(...)
+      return require("lspconfig.util").root_pattern(".git")(...)
+    end,
+  })
 end
 
 return M
