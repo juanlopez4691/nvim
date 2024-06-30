ed4fe6604906ce3caf684164d51c0a60e3a80958
Joan López <juanlopez4691@gmail.com>
2024-06-30 12:09:19 +0200

    fix: outdated configuration and keymappings for plugin folke/trouble

--- lua/core/keymappings/keys/diagnostics.lua
+++ lua/core/keymappings/keys/diagnostics.lua
@@ -30,7 +30,7 @@ return {
       mappings = {
         {
           modes = { "n" },
-          key = "<leader>de",
+          key = "<leader>df",
           cmd = "<cmd>lua vim.diagnostic.open_float()<CR>",
           opt = {
             desc = "Open float diagnostics message",
@@ -38,44 +38,35 @@ return {
         },
         {
           modes = { "n" },
-          key = "<leader>dq",
+          key = "<leader>dl",
           cmd = "<cmd>lua vim.diagnostic.setloclist()<CR>",
           opt = {
             desc = "Open diagnostics list",
           },
         },
         {
-          enabled = _G.Settings.plugins_enabled.touble,
+          enabled = _G.Settings.plugins_enabled.trouble,
           modes = { "n" },
           key = "<leader>dt",
-          cmd = "<cmd>TroubleToggle<CR>",
+          cmd = "<cmd>Trouble diagnostics toggle focus=true<CR>",
           opt = {
-            desc = "Trouble toggle",
+            desc = "Diagnostics toggle",
           },
         },
         {
-          enabled = _G.Settings.plugins_enabled.touble,
+          enabled = _G.Settings.plugins_enabled.trouble,
           modes = { "n" },
           key = "<leader>dd",
-          cmd = "<cmd>Trouble document_diagnostics<CR>",
+          cmd = "<cmd>Trouble diagnostics toggle filter.buf=0 focus=true<CR>",
           opt = {
             desc = "Document diagnostics",
           },
         },
         {
-          enabled = _G.Settings.plugins_enabled.touble,
-          modes = { "n" },
-          key = "<leader>dw",
-          cmd = "<cmd>Trouble workspace_diagnostics<CR>",
-          opt = {
-            desc = "Workspace diagnostics",
-          },
-        },
-        {
-          enabled = _G.Settings.plugins_enabled.touble,
+          enabled = _G.Settings.plugins_enabled.trouble,
           modes = { "n" },
           key = "<leader>dr",
-          cmd = "<cmd>Trouble lsp_references<CR>",
+          cmd = "<cmd>Trouble lsp_references focus=true<CR>",
           opt = {
             desc = "Symbol references",
           },
--- lua/plugins/ui/trouble.lua
+++ lua/plugins/ui/trouble.lua
@@ -2,8 +2,15 @@ return {
   "folke/trouble.nvim",
   cond = _G.Settings.plugins_enabled.trouble,
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  cmd = "TroubleToggle",
-  opts = {
-    use_diagnostic_signs = true,
-  },
+  cmd = "Trouble",
+  config = function(_, opts)
+    require("trouble").setup({
+      use_diagnostic_signs = true,
+      mode = "workspace_diagnostics", -- start in workspace diagnostics mode
+      action_keys = {
+        toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
+        -- other action keys
+      },
+    })
+  end,
 }
