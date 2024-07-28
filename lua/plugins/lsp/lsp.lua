1ad55ddc558a81d70f43184cdc9de3c90507eac6
Joan López <juanlopez4691@gmail.com>
2024-07-28 13:23:14 +0200

    refactor: mason icons configuration

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -13,7 +13,18 @@ return {
       {
         "williamboman/mason.nvim",
         config = function()
-          require("mason").setup()
+          local mason = require("mason")
+
+          -- enable mason and configure icons
+          mason.setup({
+            ui = {
+              icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+              },
+            },
+          })
         end,
       },
       { "j-hui/fidget.nvim" },
